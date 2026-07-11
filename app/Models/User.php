5abe9fc82680f0c38a4cd32f3c9ee096a01c4682
Edwin-Broski <edwin5mugisha@gmail.com>
2026-07-11 12:30:19 +0300

    added admin helper

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,15 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable, HasUuids;
 
@@ -50,6 +52,11 @@ class User extends Authenticatable
     }
 
     // helpers
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->role === 'admin' && $this->is_active;
+    }
+
     public function isAdmin(): bool
     {
         return $this->role === 'admin';
